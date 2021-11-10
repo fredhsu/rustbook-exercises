76ae03d819a780fd0330887776413171365eb98a
Fred Hsu <fredlhsu@gmail.com>
2021-11-10 14:56:40 -0800

    started chapter 10

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,16 @@ fn list_users_department(map: &HashMap<String, String>, department: &str) {
     }
 }
 
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
 fn main() {
     let mut line = String::new();
     let mut users = HashMap::new();
@@ -48,4 +58,11 @@ fn main() {
         line.clear();
         stdin = std::io::stdin().read_line(&mut line);
     }
+    // Chapter 10
+    let number_list = vec![34,50,25,100,65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let number_list = vec![102,34,6000,89,54,2,43,8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 }
