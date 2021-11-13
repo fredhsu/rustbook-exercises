d479ef03ca7e702dff0c3e8fa72ccf9eee1c83be
Fred Hsu <fredlhsu@gmail.com>
2021-11-12 16:34:06 -0800

    complete ch 10

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,26 @@ fn largest(list: &[i32]) -> i32 {
     largest
 }
 
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+
+
+
 fn main() {
     let mut line = String::new();
     let mut users = HashMap::new();
@@ -65,4 +85,17 @@ fn main() {
     let number_list = vec![102,34,6000,89,54,2,43,8];
     let result = largest(&number_list);
     println!("The largest number is {}", result);
+
+    let string1 = String::from("this string is long");
+    let result;
+    {
+    let string2 = String::from("xyz");
+    result = longest(string1.as_str(), string2.as_str());
+    println!("The longest string is {}", result);
+    }
+    let novel = String::from("Call me ishmasel. some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a .");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
